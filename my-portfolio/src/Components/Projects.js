6d977c11d32a235828d7b2a0af8a95d267c30b6c
Hassaan79 <130352083+Hassaan79@users.noreply.github.com>
2025-02-06 16:52:31 +0500

    minor changes to cards style in project section

--- my-portfolio/src/Components/Projects.js
+++ my-portfolio/src/Components/Projects.js
@@ -1,3 +1,5 @@
+// Projects.js
+
 import React from 'react';
 import './Projects.css';
 
@@ -6,6 +8,19 @@ const projects = [
     title: 'Project One',
     description: 'An innovative app that solves problem X.',
     link: 'https://github.com/yourusername/project-one',
+    image: '/images/project-one.jpg',
+  },
+  {
+    title: 'Project Two',
+    description: 'A creative solution for problem Y.',
+    link: 'https://github.com/yourusername/project-two',
+    image: '/images/project-two.jpg',
+  },
+  {
+    title: 'Project Three',
+    description: 'A unique tool addressing issue Z.',
+    link: 'https://github.com/yourusername/project-three',
+    image: '/images/project-three.jpg',
   },
   // Add more projects as needed
 ];
@@ -14,13 +29,20 @@ function Projects() {
   return (
     <section id="projects">
       <h2>Projects</h2>
-      {projects.map((project, index) => (
-        <div key={index} className="project">
-          <h3>{project.title}</h3>
-          <p>{project.description}</p>
-          <a href={project.link}>View on GitHub</a>
-        </div>
-      ))}
+      <div className="projects-container">
+        {projects.map((project, index) => (
+          <div key={index} className="project-card">
+            <div className="project-image">
+              <img src={project.image} alt={`Screenshot of ${project.title}`} />
+            </div>
+            <div className="project-content">
+              <h3>{project.title}</h3>
+              <p>{project.description}</p>
+              <a href={project.link}>View on GitHub</a>
+            </div>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
